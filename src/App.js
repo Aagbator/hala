8986adbdc271f01a1a4dffdd1bb54a9299247ff2
Anthony Agbator <tonjoe99@gmail.com>
2019-11-27 18:41:02 +0100

    completed navlink and active class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navigation from './components/navigation/navigation.component';
 import Homepage from './pages/homepage.component';
 import Footer from './components/footer/footer.component';
 
-function App() {
+function App(props) {
   return (
-    <div>
+      <Router>
       <Navigation></Navigation>
-      <Homepage></Homepage>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
       <Footer></Footer>
-    </div>
+    </Router>
   );
 }
 
--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,5 +1,6 @@
 import './navigation.styles.scss';
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
 import LineIcon from 'react-lineicons';
 import Logo from '../../../src/assets/logo.svg'
 
@@ -14,20 +15,22 @@ class Navigation extends Component {
         return(
             <div className='navigation'>
                 <div className='main-nav'>
+                    <NavLink to='/'>
                     <div className='logo'>
                         <img src={Logo} height='50' alt='logo' />
                     </div>
+                    </NavLink>
                     <nav className={`nav-menu ${this.state.isOpen ? 'is-open' : ''}`}>
                         <ul>
-                            <li><a>Ride</a></li>
-                            <li><a>Drive</a></li>
-                            <li><a>Fleet</a></li>
-                            <li><a>Business</a></li>
-                            <li><a>Freight</a></li>
+                            <li><NavLink exact activeClassName="active" to='/'>Ride</NavLink></li>
+                            <li><NavLink activeClassName="active" to='/drive'>Drive</NavLink></li>
+                            <li><NavLink activeClassName="active" to='/fleet'>Fleet</NavLink></li>
+                            <li><NavLink activeClassName="active" to='/business'>Business</NavLink></li>
+                            <li><NavLink activeClassName="active" to='/freight'>Freight</NavLink></li>
                         </ul>
                         <span onClick={this.toggleMenu} className='close-btn'><LineIcon size='sm' effect='tada' name="close" /></span>
                     </nav>
-                    <a className='get-started'>Sign Up</a>
+                    {/* <a className='get-started'>Sign Up</a> */}
                     <span onClick={this.toggleMenu} className='open-btn'><LineIcon size='md' effect='tada' name="menu" /></span>
                 </div>
             </div>
